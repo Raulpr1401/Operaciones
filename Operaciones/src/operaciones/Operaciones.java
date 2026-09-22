2ee5096549142ca831f7ff9bae73728231b7efef
Raulpr1401 <rlperezromero@gmail.com>
2026-09-22 08:54:37 +0200

    Operaciones v2

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -9,16 +9,27 @@ public class Operaciones {
 		//calculamos la suma
 		//mostramos el resultado por pantalla
 		
-		int n1= 6;
+		int n1= 3;
 		int n2= 7;
 		int n3= 8;
-		int suma = n1+n2;
+		int suma;
+		int resta;
+		int multi;
+		int divis;
+		
+		suma = n1+n2;
 		System.out.println("La suma es: " + suma);
 		
 		//a la suma le restamos 8
 		
-		suma= suma-n3;
-		System.out.println("La resta es: " + suma);
+		resta= suma-n3;
+		System.out.println("La resta es: " + resta);
+		
+		multi= suma*n1;
+		divis= suma/n2;
+		
+		System.out.println("La multiplicación es: "+multi);
+		System.out.println("La división es: "+divis);
 		
 	}
 
