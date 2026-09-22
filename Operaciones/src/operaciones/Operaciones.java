259a6ffdf280ae2f7184915f96c8a13491b57816
Raulpr1401 <rlperezromero@gmail.com>
2026-09-22 08:38:14 +0200

    Operaciones

---
+++ Operaciones/src/operaciones/Operaciones.java
@@ -0,0 +1,25 @@
+package operaciones;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		
+		
+		//declarar dos variables de tipo int con valores fijoes
+		//calculamos la suma
+		//mostramos el resultado por pantalla
+		
+		int n1= 6;
+		int n2= 7;
+		int n3= 8;
+		int suma = n1+n2;
+		System.out.println("La suma es: " + suma);
+		
+		//a la suma le restamos 8
+		
+		suma= suma-n3;
+		System.out.println("La resta es: " + suma);
+		
+	}
+
+}
